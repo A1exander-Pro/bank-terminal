bbfef463ea7deb13d42730df77678077b53e3016
A1exander-Pro <76167473+A1exander-Pro@users.noreply.github.com>
2021-01-13 16:42:43 +0300

    Add files via upload

---
+++ project/settings.py
@@ -0,0 +1,42 @@
+import os
+
+from django.conf.global_settings import DATABASES
+from dotenv import load_dotenv
+import dj_database_url
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+DATABASES['default'] = dj_database_url.parse(os.getenv("DB_URL"), conn_max_age=600)
+
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG")
+
+ROOT_URLCONF = "project.urls"
+
+ALLOWED_HOSTS = ['*']
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+    },
+]
+
+
+USE_L10N = True
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_TZ = True
